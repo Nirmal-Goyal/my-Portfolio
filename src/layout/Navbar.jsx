537685385006d719d706572663dbdafac3b084a4
Nirmal <nirmalgoyal0108@gmail.com>
2025-12-19 15:41:42 +0530

    Add resume and improve visual hierarchy

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -7,6 +7,14 @@ const Navbar = () => {
                 <li><a href="#about">About</a></li>
                 <li><a href="#projects">Projects</a></li>
                 <li><a href="#contact">Contact</a></li>
+                <li>
+                    <a href="resume/Resume.pdf"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Resume
+                    </a>
+                </li>
             </ul>
         </nav>
     )
